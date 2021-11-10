0ff638c3f2cf88ec8cf8671cef09e09060afe3b1
fernkari (∩◕-◕ )⊃━✿✿✿✿ <fernkari@amazon.com>
2021-11-09 17:47:16 -0800

    (∩◕-◕ )⊃━✿✿✿✿ Make player and enemies turn functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,38 +80,49 @@ var fightOrSkip = function() {
     return promptFight.toLowerCase();
 };
 
+var playerTurn = function(enemy) {
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
+    enemy.health = Math.max(0, enemy.health - damage);
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
+    // check enemy's health
+    if (enemy.health <= 0) {
+        window.alert(enemy.name + " has died!");
+        playerInfo.money = playerInfo.money + 20;
+        return false;
+    } else {
+        window.alert(enemy.name + " still has " + enemy.health + " health left.");
+        return true;
+    }
+};
+
+var enemyTurn = function(enemy) {
+    var damage = randomNumber(enemy.attack - 3, enemy.attack);
+    playerInfo.health = Math.max(0, playerInfo.health - damage);
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
+    // check player's health
+    if (playerInfo.health <= 0) {
+        window.alert(playerInfo.name + " has died!");
+        return false;
+    } else {
+        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+        return true;
+    }
+};
+
 var fight = function(enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
         var promptFight = fightOrSkip();
         // if player choses to fight, then fight
         if (promptFight && promptFight.trim().toLowerCase() === "fight") {
             // generate random damage value based on player's attack power
-            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
-            enemy.health = Math.max(0, enemy.health - damage);
-
-            // Log a resulting message to the console so we know that it worked.
-            console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
-            // check enemy's health
-            if (enemy.health <= 0) {
-                window.alert(enemy.name + " has died!");
-                playerInfo.money = playerInfo.money + 20;
-                break;
-            } else {
-                window.alert(enemy.name + " still has " + enemy.health + " health left.");
-            }
-
-            damage = randomNumber(enemy.attack - 3, enemy.attack);
-            playerInfo.health = Math.max(0, playerInfo.health - damage);
-
-            // Log a resulting message to the console so we know that it worked.
-            console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
-            // check player's health
-            if (playerInfo.health <= 0) {
-                window.alert(playerInfo.name + " has died!");
-                break;
-            } else {
-                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            if (playerTurn(enemy)) {
+                enemyTurn(enemy);
             }
         } else if (promptFight && promptFight.trim().toLowerCase() === "skip") {
             break;
